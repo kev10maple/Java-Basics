c1d5b46485d304472500ebbe902b8c278f169054
Kevin <kev10@umbc.edu>
2021-10-21 17:45:50 -0400

    First Commit

---
+++ JavaBasics2/src/AddMultiple.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Kevin
+ *
+ */
+public class AddMultiple {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
